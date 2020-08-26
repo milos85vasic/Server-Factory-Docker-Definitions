6bf2b0ca8f55610db4f8b541cf33bc21bd96cc4e
Milos Vasic <milos85vasic@gmail.com>
2020-08-26 14:34:53 +0200

    MSF-234 - Don't archive logs if they do not exist.

--- AntiVirus/Scripts/logrotate.sh
+++ AntiVirus/Scripts/logrotate.sh
@@ -4,10 +4,16 @@ logs=/var/log
 logFile=amavis.log
 amavisLog="${logs}/${logFile}"
 
-cp ${amavisLog} "${logs}/$(($(date +%s%N)/1000000))_$logFile"
-echo "Log initialized: `date`" > ${amavisLog}
+if test -e ${amavisLog}
+then
 
-find ${logs} -name "*_$logFile" -mtime +120 -exec rm -f {} \; >> ${amavisLog}
-echo "Beginning of log file:" >> ${amavisLog}
+  cp ${amavisLog} "${logs}/$(($(date +%s%N)/1000000))_$logFile"
+  echo "Log initialized: $(date)" > ${amavisLog}
+  find ${logs} -name "*_$logFile" -mtime +120 -exec rm -f {} \; >> ${amavisLog}
+  echo "Beginning of log file:" >> ${amavisLog}
+else
+
+  echo "Log not yet available for archiving: $(date)" >> ${amavisLog}
+fi
 
 sleep 604800; sh /logrotate.sh &
--- Dovecot/Scripts/logrotate.sh
+++ Dovecot/Scripts/logrotate.sh
@@ -9,21 +9,32 @@ dovecotLog="$logs/$logFile"
 dovecotInfoLog="$logs/$infoLogFile"
 dovecotDebugLog="$logs/$debugLogFile"
 
-cp ${dovecotLog} "${logs}/$(($(date +%s%N)/1000000))_$logFile"
-echo "Log initialized: $(date)" > ${dovecotLog}
+if test -e ${dovecotLog}
+then
 
-cp ${dovecotInfoLog} "${logs}/$(($(date +%s%N)/1000000))_$infoLogFile"
-echo "Log initialized: $(date)" > ${dovecotInfoLog}
+  cp ${dovecotLog} "${logs}/$(($(date +%s%N)/1000000))_$logFile"
+  echo "Log initialized: $(date)" > ${dovecotLog}
+  echo "Beginning of log file:" >> ${dovecotLog}
+fi
 
-cp ${dovecotDebugLog} "${logs}/$(($(date +%s%N)/1000000))_$debugLogFile"
-echo "Log initialized: $(date)" > ${dovecotDebugLog}
+if test -e ${dovecotInfoLog}
+then
+
+  cp ${dovecotInfoLog} "${logs}/$(($(date +%s%N)/1000000))_$infoLogFile"
+  echo "Log initialized: $(date)" > ${dovecotInfoLog}
+  echo "Beginning of log file:" >> ${dovecotInfoLog}
+fi
+
+if test -e ${dovecotDebugLog}
+then
+
+  cp ${dovecotDebugLog} "${logs}/$(($(date +%s%N)/1000000))_$debugLogFile"
+  echo "Log initialized: $(date)" > ${dovecotDebugLog}
+  echo "Beginning of log file:" >> ${dovecotDebugLog}
+fi
 
 find ${logs} -name "*_$logFile" -mtime +120 -exec rm -f {} \; >> ${dovecotLog}
 find ${logs} -name "*_$infoLogFile" -mtime +120 -exec rm -f {} \; >> ${dovecotInfoLog}
 find ${logs} -name "*_$debugLogFile" -mtime +120 -exec rm -f {} \; >> ${dovecotDebugLog}
-
-echo "Beginning of log file:" >> ${dovecotLog}
-echo "Beginning of log file:" >> ${dovecotInfoLog}
-echo "Beginning of log file:" >> ${dovecotDebugLog}
 
 sleep 604800; sh /logrotate.sh &
