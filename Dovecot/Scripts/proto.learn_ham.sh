61d82b41f0438bee647c29827a017daaaaebfa8a
Milos Vasic <milos85vasic@gmail.com>
2020-08-27 15:37:56 +0200

    MSF-241 - Learn ham and spam - Continued.

--- Dovecot/Scripts/proto.learn_ham.sh
+++ Dovecot/Scripts/proto.learn_ham.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-curl -X POST -H "Password: {{SERVICE.ANTI_SPAM.WEBUI.PASSWORD}}" \
---data-binary @- \
+exec curl -X POST -H "Password: {{SERVICE.ANTI_SPAM.WEBUI.PASSWORD}}" --data-binary @- \
 {{SERVICE.ANTI_SPAM.NAME}}:{{SERVICE.ANTI_SPAM.PORTS.WEBUI}}/learnham >> /var/log/dovecot/dovecot.debug.log
