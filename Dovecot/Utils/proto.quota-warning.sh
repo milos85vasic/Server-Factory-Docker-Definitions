45983d0b58c8265a9aabeea75cee0c77adfbade7
Milos Vasic <milos85vasic@gmail.com>
2020-09-16 22:49:10 +0200

    MSF-254 - Updating quota script.

--- Dovecot/Utils/proto.quota-warning.sh
+++ Dovecot/Utils/proto.quota-warning.sh
@@ -3,7 +3,7 @@
 PERCENT=$1
 USER=$2
 
-cat <<EOF | /usr/lib/dovecot/dovecot-lda -d ${USER} -o plugin/quota=maildir
+cat <<EOF | /usr/libexec/dovecot/dovecot-lda -d "${USER}" -o plugin/quota=maildir
     From: {{SERVER.POSTMASTER}}
     Subject: Quota warning
     Your mailbox is currently ${PERCENT}% full.
